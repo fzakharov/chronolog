4c453b15c94cfa8d92882fbee07808a50efaeaf6
zakharov <zakharov@signatec.ru>
2016-11-03 10:06:12 +0700

    Record wakeup event

---
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/DashboardActivity.java
@@ -0,0 +1,98 @@
+package com.revents.chronolog;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void onWakeupEventBtnClick(View v)
+    {
+        EventDbHelper mDbHelper = new EventDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(EventEntry.COLUMN_NAME_EVENT_TYPE_ID, EventEntry.EVENT_TYPE_ID_WAKEUP);
+
+        long newRowId;
+        newRowId = db.insert(EventEntry.TABLE_NAME, null, values);
+
+        mDbHelper.close();
+
+        DebugRecord();
+    }
+
+    private void DebugRecord()
+    {
+        TextView txt = (TextView) findViewById(R.id.debugTxt);
+        EventDbHelper mDbHelper = new EventDbHelper(this);
+        try {
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+            String[] projection = {
+                    EventEntry._ID,
+                    EventEntry.COLUMN_NAME_TIMESTAMP,
+                    EventEntry.COLUMN_NAME_EVENT_TYPE_ID,
+                    EventEntry.COLUMN_NAME_EVENT_TIME,
+                    EventEntry.COLUMN_NAME_EVENT_INT_VALUE,
+                    EventEntry.COLUMN_NAME_EVENT_STR_VALUE};
+
+            String sortOrder = EventEntry._ID + " DESC";
+
+            Cursor c = db.query(
+                    EventEntry.TABLE_NAME,  // The table to query
+                    projection,                               // The columns to return
+                    null,                                // The columns for the WHERE clause
+                    null,                            // The values for the WHERE clause
+                    null,                                     // don't group the rows
+                    null,                                     // don't filter by row groups
+                    sortOrder                                 // The sort order
+            );
+
+            txt.setText("");
+            CharSequence text = txt.getText();
+
+            for (int i = 0; i < c.getCount(); i++) {
+                c.moveToNext();
+
+                text = text + c.getString(c.getColumnIndex(EventEntry._ID));
+                text = text + "|";
+                text = text + c.getString(c.getColumnIndex(EventEntry.COLUMN_NAME_TIMESTAMP));
+                text = text + "|";
+                text = text + c.getString(c.getColumnIndex(EventEntry.COLUMN_NAME_EVENT_TYPE_ID));
+                text = text + "|";
+                text = text + c.getString(c.getColumnIndex(EventEntry.COLUMN_NAME_EVENT_TIME));
+                text = text + "|";
+                text = text + c.getString(c.getColumnIndex(EventEntry.COLUMN_NAME_EVENT_INT_VALUE));
+                text = text + "|";
+                text = text + c.getString(c.getColumnIndex(EventEntry.COLUMN_NAME_EVENT_STR_VALUE));
+                text = text + "|";
+
+                text = text + "\n";
+
+            }
+
+            txt.setText(text);
+
+            c.close();
+        }
+        catch (Exception ex)
+        {
+            txt.setText(ex.toString());
+        }
+        mDbHelper.close();
+    }
+}
---
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/EventDbHelperContract.java
@@ -0,0 +1,52 @@
+package com.revents.chronolog;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/* Inner class that defines the table contents */
+abstract class EventEntry implements BaseColumns {
+    public static final String TABLE_NAME = "event";
+    public static final String COLUMN_NAME_TIMESTAMP = "eventTimeStamp";
+    public static final String COLUMN_NAME_EVENT_TYPE_ID = "eventTypeId";
+    public static final String COLUMN_NAME_EVENT_TIME = "eventTime";
+    public static final String COLUMN_NAME_EVENT_INT_VALUE = "eventIntValue";
+    public static final String COLUMN_NAME_EVENT_STR_VALUE = "eventStrValue";
+    public static final int EVENT_TYPE_ID_WAKEUP = 100;
+}
+
+class EventDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 4;
+    public static final String DATABASE_NAME = "events.db";
+
+    private static final String SQL_CREATE_EVENTS =
+            "CREATE TABLE " + EventEntry.TABLE_NAME + " (" +
+                    EventEntry._ID + " INTEGER PRIMARY KEY," +
+                    EventEntry.COLUMN_NAME_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP," +
+                    EventEntry.COLUMN_NAME_EVENT_TYPE_ID + " INTEGER," +
+                    EventEntry.COLUMN_NAME_EVENT_TIME + " DATETIME DEFAULT CURRENT_TIMESTAMP," +
+                    EventEntry.COLUMN_NAME_EVENT_INT_VALUE + " INTEGER," +
+                    EventEntry.COLUMN_NAME_EVENT_STR_VALUE + " TEXT" +
+                    " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + EventEntry.TABLE_NAME;
+
+    public EventDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_EVENTS);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
