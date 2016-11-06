297afa7aa6d59c977f3ce783508fab1019add832
fzakharov <seesharp@mail.ru>
2016-11-06 23:25:34 +0700

    -

--- src/Android/Chronolog/app/src/test/java/com/revents/chronolog/ExampleUnitTest.java
+++ src/Android/Chronolog/app/src/test/java/com/revents/chronolog/ExampleUnitTest.java
@@ -1,9 +1,14 @@
 package com.revents.chronolog;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import java.io.File;
 
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static org.junit.Assert.*;
@@ -19,5 +24,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    // http://jameskbride.com/2016/02/13/android-tdd-series-test-driving-data-part-1-sqliteopenhelper.html
+
+    @Before
+    public void setUp() throws Exception {
+        String filePath = getClass().getResource("/sample.db").toURI().getPath();
+
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(
+                (new File(filePath)).getAbsolutePath(),
+                null,
+                SQLiteDatabase.OPEN_READWRITE);
+
+        // perform any db operations you want here
     }
 }
