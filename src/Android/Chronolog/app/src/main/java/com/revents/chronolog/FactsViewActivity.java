312d294cae4848469500c524e6c362093f777307
zakharov <zakharov@signatec.ru>
2016-12-08 08:23:12 +0700

    Refactoring

--- src/Android/Chronolog/app/src/main/java/com/revents/chronolog/FactsViewActivity.java
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/FactsViewActivity.java
@@ -11,7 +11,7 @@ import android.widget.ListView;
 import org.greenrobot.greendao.database.Database;
 import org.greenrobot.greendao.query.Query;
 
-import java.util.Date;
+import java.security.InvalidParameterException;
 import java.util.List;
 
 // TODO: 07.12.2016 https://github.com/greenrobot/greenDAO/blob/master/examples/DaoExample/src/main/java/org/greenrobot/greendao/example/NotesAdapter.java
@@ -31,7 +31,7 @@ public class FactsViewActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fact  fact = addFact();
+                Fact fact = addFact();
 
                 ReloadFacts();
 
@@ -43,6 +43,8 @@ public class FactsViewActivity extends AppCompatActivity {
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "fatcs-db");
         Database db = helper.getWritableDb();
         daoSession = new DaoMaster(db).newSession();
+
+        fillFactTypes();
 
         ReloadFacts();
     }
@@ -60,29 +62,51 @@ public class FactsViewActivity extends AppCompatActivity {
         lvEvents.setAdapter(adapter);
     }
 
-    private Fact addFact()
-    {
-        String coffeName = "Кофе";
-        FactType ft = new FactType();
-        FactTypeDao factTypeDao = daoSession.getFactTypeDao();
-        List list = factTypeDao.queryBuilder().where(FactTypeDao.Properties.Name.eq(coffeName)).list();
-        if (list.size() == 0)
-        {
-            ft.setName(coffeName);
-            ft.setDescription("Выпил чашку кофе");
-            factTypeDao.insert(ft);
-        }
-        else
-        {
-            ft = (FactType) list.get(0);
-        }
-
+    private Fact addFact() {
         JavaDateTimeProvider dateTimeProvider = new JavaDateTimeProvider();
         GreenDaoFactWriter wr = new GreenDaoFactWriter(dateTimeProvider, daoSession);
 
+        FactType ft = loadFactType(FACT_COFFE);
         Fact fact = new Fact(null, null, dateTimeProvider.getDate(), 1, "", ft.getId());
         wr.write(fact);
 
         return fact;
+    }
+
+    private FactType loadFactType(String name) {
+        FactTypeDao factTypeDao = daoSession.getFactTypeDao();
+        List list = factTypeDao.queryBuilder().where(FactTypeDao.Properties.Name.eq(name)).list();
+
+        if (list.size() != 1)
+            throw new InvalidParameterException("FactType count with name " + "'" + name + "': " + list.size());
+
+        return (FactType) list.get(0);
+    }
+
+    private void safeAddFactType(String name, String description) {
+        FactType ft = new FactType();
+        FactTypeDao factTypeDao = daoSession.getFactTypeDao();
+
+        List list = factTypeDao.queryBuilder()
+                .where(FactTypeDao.Properties.Name.eq(name))
+                .list();
+
+        if (list.size() > 0)
+            return;
+
+        ft.setName(name);
+        ft.setDescription(description);
+        factTypeDao.insert(ft);
+    }
+
+    String FACT_COFFE = "Кофе";
+    String FACT_WAKEUP = "Проснулся";
+    String FACT_WENT_TO_SLEEP = "Лег спать";
+
+    private void fillFactTypes() {
+
+        safeAddFactType(FACT_COFFE, "Выпил чашку кофе");
+        safeAddFactType(FACT_WAKEUP, "Подъем - качество сна 1-3");
+        safeAddFactType(FACT_WENT_TO_SLEEP, "Лег спать - состояние 1-3");
     }
 }
