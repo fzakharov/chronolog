8a8589757e496f9324cd878fb23a088ec2d14bf7
fzakharov <seesharp@mail.ru>
2016-12-10 21:19:20 +0700

    Fact type edit activity added

---
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/EditFactTypeActivity.java
@@ -0,0 +1,13 @@
+package com.revents.chronolog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditFactTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_fact_type);
+    }
+}
--- src/Android/Chronolog/app/src/main/java/com/revents/chronolog/FactTypesListActivity.java
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/FactTypesListActivity.java
@@ -1,5 +1,8 @@
 package com.revents.chronolog;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +20,20 @@ public class FactTypesListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fact_types_list);
 
         daoSession = ((App) getApplication()).getDaoSession();
 
-        setContentView(R.layout.activity_fact_types_list);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FactTypesListActivity.this, EditFactTypeActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         FactTypeDao factTypeDao = daoSession.getFactTypeDao();
         List list = factTypeDao.loadAll();
