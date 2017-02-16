a4faa98e3ff0025eaf2492cdd4837a7ab048c29c
zakharov <zakharov@signatec.ru>
2017-02-16 09:29:03 +0700

    layout

--- src/Android/Chronolog/app/src/main/java/com/revents/chronolog/features/statistics/FactTypeStatisticsActivity.java
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/features/statistics/FactTypeStatisticsActivity.java
@@ -2,8 +2,11 @@ package com.revents.chronolog.features.statistics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.revents.chronolog.R;
+import com.revents.chronolog.app.AppComponent;
+import com.revents.chronolog.app.ChronologApp;
 
 public class FactTypeStatisticsActivity extends AppCompatActivity {
 
@@ -11,5 +14,13 @@ public class FactTypeStatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fact_type_statistics);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AppComponent appComp = ((ChronologApp) getApplication()).getAppComponent();
+        appComp.inject(this);
     }
 }
