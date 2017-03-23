aed8b71643f155a6e885310a937a66ce134eb8c4
zakharov <zakharov@signatec.ru>
2017-03-23 08:21:42 +0700

    many widgets support

--- src/Android/Chronolog/app/src/main/java/com/revents/chronolog/features/statistics/StatRecyclerViewItemProvider.java
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/features/statistics/StatRecyclerViewItemProvider.java
@@ -5,11 +5,17 @@ import android.view.View;
 import com.revents.chronolog.R;
 import com.revents.chronolog.ui.recyclerview.RecyclerViewItemProvider;
 
+// TODO: 23.03.2017 Introduce map and coomon selector
 public class StatRecyclerViewItemProvider implements RecyclerViewItemProvider<Widget, WidgetRvViewHolder> {
     @Override
     public int getResourceId(Widget item) {
-        if (item.getClass() == MiddleRatingWidget.class)
+        Class<? extends Widget> aClass = item.getClass();
+
+        if (aClass == MiddleRatingWidget.class)
             return R.layout.middle_rating_widget_rv_item;
+
+        if (aClass == MiddleRatingByWeekDaysWidget.class)
+            return R.layout.middle_rating_by_week_days_widget_rv_item;
 
         throw new UnsupportedOperationException();
     }
