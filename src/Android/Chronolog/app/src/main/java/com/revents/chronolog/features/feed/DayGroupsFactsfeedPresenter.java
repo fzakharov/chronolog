c304ab9f1e529bfad5329a7fd877d41a9f5e41b4
zakharov <zakharov@signatec.ru>
2017-04-06 09:19:44 +0700

    runable

---
+++ src/Android/Chronolog/app/src/main/java/com/revents/chronolog/features/feed/DayGroupsFactsfeedPresenter.java
@@ -0,0 +1,12 @@
+package com.revents.chronolog.features.feed;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayGroupsFactsfeedPresenter implements FactsfeedPresenter {
+	@Override
+	public List<ItemPresenter> loadFactsfeed() {
+		return new ArrayList<>();
+	}
+}
